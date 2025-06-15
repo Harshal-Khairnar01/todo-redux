417a65b339bcbb62a0d2462c8e6fcfc69a96bfd7
Harshal Khairnar <harshalkhairnar7244@gmail.com>
2025-06-15 17:47:56 +0530

    update and complete feature done

--- src/slices/todoSlice.js
+++ src/slices/todoSlice.js
@@ -1,7 +1,17 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const loadFromLocalStorage = () => {
+  try {
+    const data = localStorage.getItem("todos");
+    return data ? JSON.parse(data) : [];
+  } catch (error) {
+    console.error("Failed to load from localStorage", error);
+    return [];
+  }
+};
+
 const initialState = {
-  todos: [],
+   todos: loadFromLocalStorage(),
 };
 
 const todoSlice = createSlice({
@@ -15,8 +25,23 @@ const todoSlice = createSlice({
     deleteTodo: (state, action) => {
       state.todos = state.todos.filter((todo) => todo.id !== action.payload);
     },
+    updateTodo: (state, action) => {
+      const { id, todos } = action.payload;
+      const todoIndex = state.todos.findIndex((todo) => todo.id === id);
+      if (todoIndex !== -1) {
+        state.todos[todoIndex].todo = todos;
+      }
+    },
+    toggleComplete: (state, action) => {
+      const { id, complete } = action.payload;
+      const todoIndex = state.todos.findIndex((todo) => todo.id === id);
+      if (todoIndex !== -1) {
+        state.todos[todoIndex].complete = complete;
+      }
+    },
   },
 });
 
-export const { addTodo, deleteTodo } = todoSlice.actions;
+export const { addTodo, deleteTodo, updateTodo, toggleComplete } =
+  todoSlice.actions;
 export default todoSlice.reducer;
--- src/store/index.js
+++ src/store/index.js
@@ -2,8 +2,21 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import todoReducer from "../slices/todoSlice";
 
+const saveToLocalStorage = (state) => {
+  try {
+    const serializedState = JSON.stringify(state.todos.todos); // access todos slice
+    localStorage.setItem("todos", serializedState);
+  } catch (error) {
+    console.error("Could not save state", error);
+  }
+};
+
 export const store = configureStore({
   reducer: {
     todos: todoReducer,
   },
+});
+
+store.subscribe(() => {
+  saveToLocalStorage(store.getState());
 });
